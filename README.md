0554919788990371afe5eec6971ec0f7f7e9c396
franciscoramon-html <francisco.ramon@empas.mvl.edu.ar>
2025-10-04 11:45:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-gh7ng1lw
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/franciscoramon-html/stackblitz-starters-gh7ng1lw)
---
+++ script.js
@@ -0,0 +1,30 @@
+
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
+
+
+const mobileLinks = mobileMenu.getElementsByTagName('a');
+for (let link of mobileLinks) {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.add('hidden');
+    });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
